98a0e447e11ed285ff5f448dbbac7e6234cc8564
onyiafranklin <onyiafranklin89@gmail.com>
2023-04-13 22:41:35 -0400

    Optimized production runtime

--- src/index.js
+++ src/index.js
@@ -2,23 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.css";
-import reportWebVitals from './reportWebVitals';
 import AuthProvider from "./Context/AuthContext/AuthContext";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { clientID } from "./Routes/url";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId={clientID}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </GoogleOAuthProvider>
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId={clientID}>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </GoogleOAuthProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
